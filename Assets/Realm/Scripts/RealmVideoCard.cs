4327f704681d2cbe8db454aede210fa3fff03237
Iurii Pavliuk <iurii@playsnak.com>
2017-11-13 12:18:00 +0100

    - clean up

--- Assets/Realm/Scripts/RealmVideoCard.cs
+++ Assets/Realm/Scripts/RealmVideoCard.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VideoCard : MonoBehaviour {
+public class RealmVideoCard : MonoBehaviour {
 
 	[SerializeField]
 	private YouTubePlaylist _playlist;
